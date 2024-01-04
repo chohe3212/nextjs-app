cb5bd911242b1c44c7de5be808b071e646f9733a
HyoEun Cho <chohe3212@naver.com>
2024-01-05 00:08:54 +0900

    prisma, postgres 설정

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -8,16 +8,16 @@ const NavItem = ({ moblie }: { moblie?: boolean }) => {
         moblie && "flex-col h-full"
       }`}
     >
-      <li className="py-2 text-center border-b-4 cursor-pointer">
+      <li className="py-1 text-center border-b-4 cursor-pointer">
         <Link href="/admin">Admin</Link>
       </li>
-      <li className="py-2 text-center border-b-4 cursor-pointer">
+      <li className="py-1 text-center border-b-4 cursor-pointer">
         <Link href="/user">User</Link>
       </li>
-      <li className="py-2 text-center border-b-4 cursor-pointer">
+      <li className="py-1 text-center border-b-4 cursor-pointer">
         <button>Signout</button>
       </li>
-      <li className="py-2 text-center border-b-4 cursor-pointer">
+      <li className="py-1 text-center border-b-4 cursor-pointer">
         <button>Signin</button>
       </li>
     </ul>
