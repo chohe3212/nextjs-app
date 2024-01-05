c24a84154a3d66bda6cde7266e5b3ba367959904
HyoEun Cho <chohe3212@naver.com>
2024-01-05 23:16:33 +0900

    css 수정

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     <nav className="relative z-10 w-full bg-orange-400 text-white">
       <div className="flex items-center justify-between mx-5 sm:mx-10 lg:mx-20">
         <div className="flex items-center text-2xl h-14">
-          <Link href="/">Logo</Link>
+          <Link href="/">Logo </Link>
         </div>
         <div className="text-2xl sm:hidden">
           {menu === false ? (
