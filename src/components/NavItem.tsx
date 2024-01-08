2cea5ffc6e07bafc94c088136f0ba61a74437d0c
HyoEun Cho <chohe3212@naver.com>
2024-01-08 23:04:43 +0900

    next-auth 설정 및 로그인 로그아웃 구현

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const NavItem = ({ moblie }: { moblie?: boolean }) => {
+  const { data: session, status } = useSession();
+
+  console.log(session, status);
+  // 세션에 따라 보여질 메뉴를 다르게 하기 위해.
+
   return (
     <ul
       className={`text-md justify-center flex gap-4 w-full item-center ${
@@ -14,12 +20,15 @@ const NavItem = ({ moblie }: { moblie?: boolean }) => {
       <li className="py-1 text-center border-b-4 cursor-pointer">
         <Link href="/user">User</Link>
       </li>
-      <li className="py-1 text-center border-b-4 cursor-pointer">
-        <button>Signout</button>
-      </li>
-      <li className="py-1 text-center border-b-4 cursor-pointer">
-        <button>Signin</button>
-      </li>
+      {session?.user ? (
+        <li className="py-1 text-center border-b-4 cursor-pointer">
+          <button onClick={() => signOut()}>Signout</button>
+        </li>
+      ) : (
+        <li className="py-1 text-center border-b-4 cursor-pointer">
+          <button onClick={() => signIn()}>Signin</button>
+        </li>
+      )}
     </ul>
   );
 };
